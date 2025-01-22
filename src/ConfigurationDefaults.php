a87380c584cf289e725db7e26152654901d0ab80
eden <eyw520@gmail.com>
2025-01-22 12:40:00 -0500

    Bump Square Version.

--- src/ConfigurationDefaults.php
+++ src/ConfigurationDefaults.php
@@ -27,7 +27,7 @@ class ConfigurationDefaults
 
     public const HTTP_METHODS_TO_RETRY = ['GET', 'PUT'];
 
-    public const SQUARE_VERSION = '2024-12-18';
+    public const SQUARE_VERSION = '2025-01-23';
 
     public const ADDITIONAL_HEADERS = [];
 
